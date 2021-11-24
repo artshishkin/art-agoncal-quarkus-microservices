a8dcc93957975863a517da6dae74b363c25d98a2
Art <d.art.shishkin@gmail.com>
2021-11-24 18:21:29 +0200

    36. Testing the Book Microservice (#3)

--- rest-book/src/test/java/net/shyshkin/study/quarkus/microservices/book/BookResourceTest.java
+++ rest-book/src/test/java/net/shyshkin/study/quarkus/microservices/book/BookResourceTest.java
@@ -3,19 +3,41 @@ package net.shyshkin.study.quarkus.microservices.book;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 public class BookResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void shouldCreateBook() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
+                .formParams(
+                        "title", "My great book",
+                        "year", 2020,
+                        "author", "Art Shyshkin",
+                        "genre", "Fantasy"
+                )
+
+                .when().post("/api/books")
+
+                .then()
+                .statusCode(201)
+                .body("author", is("Art Shyshkin"))
+                .body("genre", is("Fantasy"))
+                .body("isbn_13", is("We need to get ISBN from external service"))
+                .body("title", is("My great book"))
+                .body("year_of_publication", is(2020))
+                .body("creation_date", is(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"))))
+                .body("creation_date", startsWith("20"))
+        ;
     }
 
 }
