3e4a4c2c9e4902dd4863042440dbcf83badf13fc
Art <d.art.shishkin@gmail.com>
2021-11-26 10:03:28 +0200

    44.2 Tuning retry strategy (#4)

--- rest-book/src/main/java/net/shyshkin/study/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/net/shyshkin/study/quarkus/microservices/book/BookResource.java
@@ -4,7 +4,9 @@ import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -13,6 +15,9 @@ import javax.ws.rs.core.Response;
 @Path("/api/books")
 @Tag(name = "Book REST endpoint")
 public class BookResource {
+
+    @Inject
+    Logger logger;
 
     private final BookService bookService;
     private final FallbackBookService fallback;
@@ -30,16 +35,21 @@ public class BookResource {
             description = "Creates a Book with an ISBN number"
     )
     @Fallback(fallbackMethod = "fallbackCreateABook")
-    @Retry(delay = 2000, maxRetries = 1)
+    @Retry(delay = 900, maxRetries = 2)
     public Response createABook(
             @FormParam("title") String title,
             @FormParam("author") String author,
             @FormParam("year") int yearOfPublication,
             @FormParam("genre") String genre) {
 
-        Book book = bookService.createABook(title, author, yearOfPublication, genre);
-
-        return Response.status(201).entity(book).build();
+        try {
+            logger.info("Trying to create a book");
+            Book book = bookService.createABook(title, author, yearOfPublication, genre);
+            return Response.status(201).entity(book).build();
+        } catch (Exception e) {
+            logger.warn(e.getMessage());
+            throw e;
+        }
     }
 
     public Response fallbackCreateABook(
